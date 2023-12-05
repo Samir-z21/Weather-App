b26a0552d14a9c95f6707875f9ddcc25bd6ce90b
Samir <samir_zulfiqar@yahoo.com>
2023-12-05 15:44:15 -0500

    added error handling

--- src/index.js
+++ src/index.js
@@ -26,15 +26,37 @@ function Weather(city, country, lastUploaded, weatherPic, tempC, tempF, textDesc
 
 
 function fetchAPI() {
-  fetch(`https://api.weatherapi.com/v1/current.json?key=e7a5e7444ffd4d0087314554232211&q=${chosenCity}`,{mode:'cors'}) 
- .then((response) => response.json())
- .then((response) => {
-    console.log(response);
-     currentWeather = new Weather (response.location.name, response.location.country, response.current.last_updated, response.current.condition.icon, response.current.temp_c, response.current.temp_f, response.current.condition.text, response.current.feelslike_c, response.current.feelslike_f, response.current.humidity, response.current.wind_kph);
-    
-    displayWeather (currentWeather)
-  });
+  fetch(`https://api.weatherapi.com/v1/current.json?key=e7a5e7444ffd4d0087314554232211&q=${chosenCity}`, { mode: 'cors' }) 
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(response => {
+      console.log(response);
+      currentWeather = new Weather (
+        response.location.name,
+        response.location.country,
+        response.current.last_updated, 
+        response.current.condition.icon, 
+        response.current.temp_c, 
+        response.current.temp_f, 
+        response.current.condition.text, 
+        response.current.feelslike_c, 
+        response.current.feelslike_f, 
+        response.current.humidity, 
+        response.current.wind_kph
+      );
+      displayWeather(currentWeather);
+    })
+    .catch(error => {
+      // Handle network errors and other issues
+      alert(`Error during fetch: No matching location found.`);
+      console.error('Error during fetch:', error);
+    });
 }
+
 
 
 fetchAPI()
