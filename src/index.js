54585aea6df318bf31a30653b8ecd15ef09e77bc
Samir <samir_zulfiqar@yahoo.com>
2023-12-05 14:38:19 -0500

    added icon of weather, input can now take in values and fetch and display API, fixed C-F conversion bugs

--- src/index.js
+++ src/index.js
@@ -1,39 +1,55 @@
 import './style.css'
 import {displayWeather, changeDegrees} from './loadContent'
 
-const celsiusFahrenheit = document.querySelector('.celsiusFahrenheit')
+const celsiusFahrenheit = document.querySelector('.celsiusFahrenheit');
+const searchContainer = document.querySelector('.searchContainer');
+const searchLocation = document.querySelector('.searchLocation');
 
 
-const chosenCity = "saint-laurent"
+let chosenCity = "Montreal"
+let currentWeather;
+let fetchedAPI = false; 
 
-function Weather(city, country, lastUploaded, tempC, tempF, weatherDescription, feelsLikeC, feelsLikeF, humidity, wind) {
+function Weather(city, country, lastUploaded, weatherPic, tempC, tempF, textDescription, feelsLikeC, feelsLikeF, humidity, wind) {
     this.city = city;
     this.country = country;
     this.lastUploaded = lastUploaded;
+    this.weatherPic = weatherPic;
     this.tempC = tempC;
     this.tempF = tempF; 
-    this.weatherDescription = weatherDescription;
+    this.textDescription = textDescription;
     this.feelsLikeC = feelsLikeC;
     this.feelsLikeF = feelsLikeF;
     this.humidity = humidity;
     this.wind = wind;
 }
 
-fetch(`https://api.weatherapi.com/v1/current.json?key=e7a5e7444ffd4d0087314554232211&q=${chosenCity}`,{mode:'cors'}) 
+
+function fetchAPI() {
+  fetch(`https://api.weatherapi.com/v1/current.json?key=e7a5e7444ffd4d0087314554232211&q=${chosenCity}`,{mode:'cors'}) 
  .then((response) => response.json())
  .then((response) => {
     console.log(response);
-    const currentWeather = new Weather (response.location.name, response.location.country, response.current.last_updated, response.current.temp_c, response.current.temp_f, response.current.condition.text, response.current.feelslike_c, response.current.feelslike_f, response.current.humidity, response.current.wind_kph);
+     currentWeather = new Weather (response.location.name, response.location.country, response.current.last_updated, response.current.condition.icon, response.current.temp_c, response.current.temp_f, response.current.condition.text, response.current.feelslike_c, response.current.feelslike_f, response.current.humidity, response.current.wind_kph);
     
     displayWeather (currentWeather)
-
-    celsiusFahrenheit.addEventListener ('click', changeDegreesHandler);
-
-    function changeDegreesHandler() {
-      changeDegrees(currentWeather)
-    } 
-    console.log(currentWeather)
   });
+}
 
 
+fetchAPI()
  
+searchContainer.addEventListener('submit', e => {
+  e.preventDefault();
+  chosenCity = searchLocation.value;
+  fetchAPI();
+  searchLocation.value = "";
+  fetchedAPI = true;
+  changeDegrees(currentWeather, fetchedAPI);
+})
+
+
+celsiusFahrenheit.addEventListener ('click', () => {
+  changeDegrees(currentWeather)
+});
+
--- src/loadContent.js
+++ src/loadContent.js
@@ -2,12 +2,13 @@
 const countriesList = require('countries-list');
 
 
-const currentWeatherContainer = document.querySelector('#currentWeatherContainer');
+// const currentWeatherContainer = document.querySelector('#currentWeatherContainer');
 const cityCountry = document.querySelector('.cityCountry');
 const uploadDate = document.querySelector('.uploadDate');
-const weather = document.querySelector('.weather');
-const description = document.querySelector('.description');
-const details = document.querySelector('.details');
+const weatherDegrees = document.querySelector('.weatherDegrees');
+const weatherPic = document.querySelector('.weatherPic');
+const textDescription = document.querySelector('.textDescription');
+// const details = document.querySelector('.details');
 const feelsLike = document.querySelector('.feelsLike');
 const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
@@ -19,12 +20,13 @@ const fahrenheitBtn = document.querySelector('.fahrenheitBtn');
 
 celsiusBtn.classList.add('activeDegree');
 
+console.log(countriesList)
 
 function findKeyByCountryName(countryName) {
     for (const key in countriesList.countries) {
-      if (countriesList.countries[key].name === countryName) {
+      if ((countriesList.countries[key].name === countryName) || (countriesList.countries[key].name.includes(countryName)) || countryName.includes(countriesList.countries[key].name)){
         return key;
-      }
+      } 
     }
     // If the country name is not found
     return null;
@@ -36,25 +38,39 @@ function displayWeather (currentWeather) {
 
     cityCountry.textContent = `${currentWeather.city},${foundKey}`;
     uploadDate.textContent = currentWeather.lastUploaded;
-    weather.textContent = `${Math.round(currentWeather.tempC)}°C`;
-    description.textContent = currentWeather.weatherDescription;
+    weatherDegrees.textContent = `${Math.round(currentWeather.tempC)}°C`;
+    weatherPic.src = `https:${currentWeather.weatherPic}`
+    textDescription.textContent = currentWeather.textDescription;
+    feelsLike.textContent = `Feels like: ${Math.round(currentWeather.feelsLikeC)}°`
+    humidity.textContent = `Humidity levels: ${currentWeather.humidity}%`
+    wind.textContent = `Wind: ${currentWeather.wind}km/h`
 }
 
 
 
-function changeDegrees(currentWeather) {
-    if (weather.textContent.includes("°C")) {
-        weather.textContent = `${Math.round(currentWeather.tempF)}°F`;
+function changeDegrees(currentWeather, fetchedAPI) {
+  if (fetchedAPI) {
+    weatherDegrees.textContent = `${Math.round(currentWeather.tempC)}°C`;
+    feelsLike.textContent = `Feels like: ${Math.round(currentWeather.feelsLikeC)}°`
+    fahrenheitBtn.classList.remove('activeDegree');
+    celsiusBtn.classList.add('activeDegree');
+    celsiusFahrenheit.style.backgroundColor = "rgb(204,204,204)";
+    return
+  }
+    if (weatherDegrees.textContent.includes("°C")) {
+        weatherDegrees.textContent = `${Math.round(currentWeather.tempF)}°F`;
+        feelsLike.textContent = `Feels like: ${Math.round(currentWeather.feelsLikeF)}°`
         celsiusBtn.classList.remove('activeDegree');
         fahrenheitBtn.classList.add('activeDegree');
         celsiusFahrenheit.style.backgroundColor = "rgb(51,51,51)";
     } else {
-        weather.textContent = `${Math.round(currentWeather.tempC)}°C`;
+        weatherDegrees.textContent = `${Math.round(currentWeather.tempC)}°C`;
+        feelsLike.textContent = `Feels like: ${Math.round(currentWeather.feelsLikeC)}°`
         fahrenheitBtn.classList.remove('activeDegree');
         celsiusBtn.classList.add('activeDegree');
         celsiusFahrenheit.style.backgroundColor = "rgb(204,204,204)";
-
     }
 }
+
 
 export {displayWeather, changeDegrees}
